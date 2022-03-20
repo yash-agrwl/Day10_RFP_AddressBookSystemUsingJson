975435de8501383586581218880d429af2e26313
yash-agrwl <agarwalyash7012@gmail.com>
2022-03-20 15:11:25 +0530

    added empty project file with welcome message

---
+++ AddressBookSystem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AddressBookSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Computation Of Address Book System Problem");
+        }
+    }
+}
